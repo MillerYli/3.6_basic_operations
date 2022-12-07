3349f6102544dd60e547a896cfe1098783ee7c7a
Yulia Miller <miller82.98@mail.ru>
2022-12-06 20:45:49 -0800

    Добавили операцию остаток от деления

--- 3.6_basic_operations/Program.cs
+++ 3.6_basic_operations/Program.cs
@@ -8,8 +8,11 @@ namespace MyNameSpase
         {
             double a = 5, b = 2;
 
-            double result = a / b;
+            double result = a / b;      //Операция деления
             Console.WriteLine("{0}/{1}={2}", a, b, result);
+
+            double result_remains = a % b;
+            Console.WriteLine("{0} % {1} = {2}", a, b, result_remains);
         }
     }
 }
