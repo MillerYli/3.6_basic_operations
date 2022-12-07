7b3f09f36994e8be2ad72f19b145d263bd17ae1a
Yulia Miller <miller82.98@mail.ru>
2022-12-06 20:38:04 -0800

    Добавьте файлы проекта.

---
+++ 3.6_basic_operations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyNameSpase
+{
+    class myclass
+    {
+        public static void Main(string[] args)
+        {
+            double a = 5, b = 2;
+
+            double result = a / b;
+            Console.WriteLine("{0}/{1}={2}", a, b, result);
+        }
+    }
+}
